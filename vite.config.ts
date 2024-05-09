b41a28ed31610879ee03b030c46ff9e31919b2f3
Fahad <fahadyounas97@outlook.com>
2024-05-09 21:01:33 +0500

    Added base property to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
-})
+});
+
+// // vite.config.ts
+
+// import { defineConfig } from "vite";
+// // import { resolve } from "path";
+
+// export default defineConfig({
+
+//   build: {
+//     outDir: "dist",
+//     assetsDir: "",
+//     sourcemap: true,
+//   },
+//   resolve: {
+//     extensions: [".ts", ".js", ".scss"],
+//   },
+//   server: {
+//     open: true,
+//   },
+// });
